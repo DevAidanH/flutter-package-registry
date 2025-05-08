8dbea59635fcb04c6a4aafd9792d726e9772727e
DevAidan <aidanhumpidge333@gmail.com>
2025-05-08 12:28:43 +0100

    progile page implemented with favoriates

--- v2/flutter-package-registry/src/pages/profile.jsx
+++ v2/flutter-package-registry/src/pages/profile.jsx
@@ -1,17 +1,45 @@
-import { auth } from "../firebase-config";
+import { auth, db } from "../firebase-config";
 import React, { useEffect, useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 function Profile(){
 
+    const [favoritesList, setfavoritesList] = useState([]);
     const [name, setName] = useState("");
+
+    const packagesCollectionsRef = collection(db, "users");
 
     useEffect (() => {
             const name =  auth.currentUser.displayName
             setName(name)
+
+            const getFavorites = async () => {
+                const q = query(packagesCollectionsRef, where("uid", "==", auth.currentUser.uid));
+                const data = await getDocs(q);
+                if (!data.empty) {
+                    setFavoritesList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+                } else {
+                    setFavoritesList([]);
+                }
+              };
+              
+              if (auth.currentUser) {
+                getFavorites();
+              }
         }, []);
 
     return <div>
         <h1>Welcome {name}</h1>
+        <h2>Your Favorites: </h2>
+        {favoritesList.length > 0 ? (
+            <ul>
+            {favoritesList.map((favorite) => (
+                <li key={favorite.id}>{favorite.name}</li>
+            ))}
+            </ul>
+        ) : (
+            <p>No favorites found yet.</p>
+        )}
     </div>;
 }
 
